e8736765fa095a250bdfdbd3bc8421b121893305
Eric Fong <emfong@ignisphaseone.com>
2012-09-07 12:36:09 -0700

    added djinn DB management for save/restore information

---
+++ src/oath/db/djinn.py
@@ -0,0 +1,26 @@
+def db_connect():
+    pass
+
+
+def get_guard():
+    pass
+
+
+def get_warden():
+    pass
+
+
+def get_sentry():
+    pass
+
+
+def save_guard():
+    pass
+
+
+def save_warden():
+    pass
+
+
+def save_sentry():
+    pass
